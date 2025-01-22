4219b4d311b4d315994683c9e79e2c13b24fe7d5
rohithyd8 <ryd06733@gmail.com>
2025-01-22 14:45:40 +0530

    just add comments

--- src/main/java/com/jewelleryshop/controller/AdminOrderController.java
+++ src/main/java/com/jewelleryshop/controller/AdminOrderController.java
@@ -27,6 +27,7 @@ public class AdminOrderController {
 		this.orderService=orderService;
 	}
 	
+	//getting all order handler
 	@GetMapping("/")
 	public ResponseEntity<List<Order>> getAllOrdersHandler(){
 		List<Order> orders=orderService.getAllOrders();
